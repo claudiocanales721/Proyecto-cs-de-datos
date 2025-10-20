816278ec1d845fb2677e9f2cd856e82d08d96445
stingrayhub <aaqueveque@estudiante.uc.cl>
2025-10-20 12:32:51 -0300

    proceso game stop api

---
+++ Partes_separadas_del_proyecto/Alonso/gamespot_api.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+import requests
+import json
+from time import sleep
+
+
+doc="Partes_separadas_del_proyecto/Alonso/llave.txt"
+
+def gamespotapi (llave, sesion, offset):
+
+    url='http://www.gamespot.com/api/reviews/?'
+
+    parametros = {
+        'api_key': llave, 
+        'format': 'json',
+        'offset': f'{offset}'
+    }
+
+    for _ in range(10):
+        try:
+            respuesta = sesion.get(f'{url}', headers= sesion.headers, params=parametros)
+            respuesta.raise_for_status()
+            #print(f'{respuesta.text}\n\n\n' )
+            return respuesta.json()
+        
+        except requests.exceptions.HTTPError as e:
+            print("HTTPError:", e)
+            sleep(6)
+
+        except requests.exceptions.Timeout:
+            print("Timeout alcanzado.")
+            sleep(6)
+
+        except requests.exceptions.RequestException as e:
+            print("Error de red:", e)
+            sleep(6)
+
+        
+
+
+
+with open(doc, mode="r", encoding="utf-8") as lista:
+    llave = lista.readline().rstrip()
+
+sesion= requests.session()
+sesion.headers.update({'User-Agent': 'stingray49 PUC proyect https://github.com/claudiocanales721/Proyecto-cs-de-datos (aaqueveque@estudante.uc.cl)'})
+
+
+offset=15800
+no_ultimo=True
+contador=159
+while no_ultimo:
+    respuesta=gamespotapi(llave,sesion,offset)
+    juegos=respuesta['results']
+
+    if len(juegos) < 100:
+        no_ultimo=False
+
+    with open('data/gamespot_reseñas.json', 'w', encoding='utf-8') as f:
+        json.dump(juegos, f, indent=4)
+    
+    offset+=100
+    print(f'Se guardó el request N°{contador}, con {len(juegos)} juegos, en total hay {offset} juegos')
+    contador+=1
+
+
+
